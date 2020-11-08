8a630a076cc6e16a7cf0ceef43841aa7978a34fe
yukie <babyblueloud@gmail.com>
2020-11-08 17:47:59 +0900

    first commit

---
+++ fizzbuzz.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * fizzbuzz.php
+ **/
+echo "************ FizzBuzz!! ************\n\n";
+
+for($i = 1; $i <= 100; $i++) {
+	if(($i % 3 == 0) && ($i % 5 == 0)) {
+		echo "FizzBuzz\n";
+	}
+	elseif($i % 3 ==0) {
+		echo "Fizz\n";
+	}
+	elseif($i % 5 == 0) {
+		echo "Buzz\n";
+	}
+	else {
+		echo $i . "\n";
+	}
+}
